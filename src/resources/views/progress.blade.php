2b9e784c8556fd573e909739a4e09b0e8346edad
Tiger Fok <tiger@tiger-workshop.com>
2018-09-06 23:56:43 +0800

    Updated progress view use trans()

--- src/resources/views/progress.blade.php
+++ src/resources/views/progress.blade.php
@@ -1,41 +1,41 @@
 @extends("admin.layout")
 
-@section('page-title', trans("laravel-job-status::progress.title", ['name' => $status->type]))
+@section('page-title', trans("laravel-job-status::progress.title", ['name' => trans($status->type)]))
 
 @push('breadcrumb')
-    <li class="breadcrumb-item">{{ trans("laravel-job-status::progress.title", ['name' => $status->type]) }}</li>
+    <li class="breadcrumb-item">{{ trans("laravel-job-status::progress.title", ['name' => trans($status->type)]) }}</li>
 @endpush
 
 @section('content')
     @if ($status->status == 'queued')
         <div class="alert alert-info">
-            {{ trans("laravel-job-status::progress.submitted", ['name' => $status->type]) }}
+            {{ trans("laravel-job-status::progress.submitted", ['name' => trans($status->type)]) }}
         </div>
     @elseif ($status->status == 'executing')
         <div class="alert alert-info">
-            {{ trans("laravel-job-status::progress.processing", ['name' => $status->type]) }}
+            {{ trans("laravel-job-status::progress.processing", ['name' => trans($status->type)]) }}
         </div>
     @elseif ($status->status == 'finished')
         @if (isset($status->output['success_rows']))
             @if ($status->output['failed_rows'] == 0)
                 <div class="alert alert-success">
-                    <p>{{ trans("laravel-job-status::progress.finished", ['name' => $status->type]) }}</p>
+                    <p>{{ trans("laravel-job-status::progress.finished", ['name' => trans($status->type)]) }}</p>
                     <p>{{ sprintf(trans("laravel-job-status::progress.result_numbers"), $status->output['total_rows'], $status->output['success_rows'], $status->output['skipped_rows'], $status->output['failed_rows']) }}</p>
                 </div>
             @elseif ($status->output['success_rows'] == 0)
                 <div class="alert alert-danger">
-                    <p>{{ trans("laravel-job-status::progress.all_failed", ['name' => $status->type]) }}</p>
+                    <p>{{ trans("laravel-job-status::progress.all_failed", ['name' => trans($status->type)]) }}</p>
                     <p>{{ sprintf(trans("laravel-job-status::progress.result_numbers"), $status->output['total_rows'], $status->output['success_rows'], $status->output['skipped_rows'], $status->output['failed_rows']) }}</p>
                 </div>
             @else
                 <div class="alert alert-warning">
-                    <p>{{ trans("laravel-job-status::progress.partially_finished", ['name' => $status->type]) }}</p>
+                    <p>{{ trans("laravel-job-status::progress.partially_finished", ['name' => trans($status->type)]) }}</p>
                     <p>{{ sprintf(trans("laravel-job-status::progress.result_numbers"), $status->output['total_rows'], $status->output['success_rows'], $status->output['skipped_rows'], $status->output['failed_rows']) }}</p>
                 </div>
             @endif
         @else
             <div class="alert alert-success">
-                <p>{{ trans("laravel-job-status::progress.finished", ['name' => $status->type]) }}</p>
+                <p>{{ trans("laravel-job-status::progress.finished", ['name' => trans($status->type)]) }}</p>
             </div>
         @endif
     @elseif ($status->status == 'failed')
